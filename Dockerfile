42c320725e692af25a125dd6a86f43194b815ae8
gbvsilva <graco.babeuf18@gmail.com>
2022-05-31 15:30:41 -0300

    Change to alpine base and clear cache msgs logic

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
-FROM python:3.7-buster
+FROM alpine
 
 RUN mkdir /opt/telegram-automate
 COPY . /opt/telegram-automate
 
 WORKDIR /opt/telegram-automate
 
-RUN apt update
-RUN apt install nocache -y python3-pip git
-RUN pip install -r requirements.txt
+RUN apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python
+RUN apk add --no-cache git
+RUN python3 -m ensurepip
+RUN pip3 install --no-cache --upgrade pip setuptools && pip3 install -r requirements.txt
 
 RUN git clone https://github.com/gbvsilva/Telethon.git
 RUN cd Telethon && python setup.py install
--- roletas/copy_app.py
+++ roletas/copy_app.py
@@ -1,4 +1,4 @@
-from calendar import c
+import asyncio
 from tools.credentials import Client
 from telethon import events
 from rules import Rule
@@ -11,46 +11,44 @@ class CopyApp():
         self.target_channels = target_channels
 
         # Messages to delete
-        self.msgs_to_delete_ids = {}
         self.msgs_to_delete_list = {}
         # Setting rules
         for channel in self.target_channels:
             rule_name = self.target_channels[channel]['rule']
             self.target_channels[channel]['rule'] = Rule(rule_name)
-            self.msgs_to_delete_ids[channel] = {}
             self.msgs_to_delete_list[channel] = []
+        # Text Filters
+        self.msg_to_delete_filters = ['ANALISANDO', 'Fazer gale']
+        self.msg_to_maintain_filters = ['WIN', 'RED', 'ANÁLISE CONFIRMADA']
+        self.msg_filters = self.msg_to_delete_filters + self.msg_to_maintain_filters
 
         @Client.on(events.NewMessage(chats=self.source_channel))
         async def new_message(event):
             print(f'=== Roletas Nova Mensagem ID -> {event.id} ===\n'+event.raw_text)
-            if 'ANÁLISE CONFIRMADA' in event.raw_text or 'WIN' in event.raw_text or 'RED' in event.raw_text:
+            if bool([text for text in self.msg_filters if text in event.raw_text]):
                 for channel in self.target_channels:
                     ok = await self.target_channels[channel]['rule'].execute()
                     if ok:
                         message = await Client.send_message(channel, event.raw_text)
                         print(f"Mensagem enviada para {self.target_channels[channel]['name']} ID -> {message.id}")
-            elif 'ANALISANDO' in event.raw_text or 'Fazer gale' in event.raw_text:
-                for channel in self.target_channels:
-                    ok = await self.target_channels[channel]['rule'].execute()
-                    if ok:
-                        message = await Client.send_message(channel, event.raw_text)
-                        print(f"Mensagem enviada para {self.target_channels[channel]['name']} ID -> {message.id}")
-                        self.msgs_to_delete_ids[channel][event.id] = message.id
-
+                        if bool([text for text in self.msg_to_delete_filters if text in event.raw_text]):
+                            self.msgs_to_delete_list[channel].append(message.id)
+        
+        # This method is not 100% reliable. Then, all messages with 'ANALISANDO' and 'Fazer gale' will be always cleared
         @Client.on(events.MessageDeleted(chats=[self.source_channel]))
         async def insert_into_del_list(event):
             print(f'=== Inserindo nas filas mensagens a serem excluídas IDs -> {event.deleted_ids} ===')
             for channel in self.target_channels:
                 for message_id in event.deleted_ids:
-                    self.msgs_to_delete_list[channel].append(message_id)
+                    if message_id not in self.msgs_to_delete_list[channel]:
+                        self.msgs_to_delete_list[channel].append(message_id)
             
 
     async def clear_messages_to_delete(self):   
         # Clearing IDs in the list to delete
+        await asyncio.sleep(60)
+        print('=== Limpando cache de mensagens ===')
         for channel in self.target_channels:
             for message_id in self.msgs_to_delete_list[channel]:
-                if message_id in self.msgs_to_delete_ids[channel]:
-                    await Client.delete_messages(channel, self.msgs_to_delete_ids[channel][message_id])
-                    del self.msgs_to_delete_ids[channel][message_id]
-                    self.msgs_to_delete_list[channel].remove(message_id)
-            
+                await Client.delete_messages(channel, message_id)
+                self.msgs_to_delete_list[channel].remove(message_id)
